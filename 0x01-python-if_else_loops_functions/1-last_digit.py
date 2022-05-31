d229a4a121d31843222dd60e9eed2d1893b55a43
Dave-Olo <destineddave@gmail.com>
2022-05-31 21:01:50 +0100

    Last Figure

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str1 = " Last digit of"
+str1 = "Last digit of"
 greater = "and is greater than 5"
 zero = "and is 0"
 less_than= "and is less than 6 and not 0"
