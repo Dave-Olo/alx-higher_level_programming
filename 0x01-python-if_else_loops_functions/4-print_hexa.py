87c205d83594dbbf4f156ebd0d1f39e0f6bb7c50
Dave-Olo <destineddave@gmail.com>
2022-05-31 21:43:31 +0100

    0 t0 98 in decimal and hexaecimal

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Printing numbers from 0 to 98 in decimal(base 10) and hexadecimal(base 16)
-for number in range(0,99):
-    print(f"{number:d} = 0x{number:x}")
+for num in range(0, 99):
+    print("{:d} = 0x{:x}".format(num, num))
