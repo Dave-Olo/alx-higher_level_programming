86bb85ae199994ffa944af9fa703dbeefee6a73a
Dave-Olo <destineddave@gmail.com>
2022-05-31 20:57:06 +0100

    Last digit verified

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,7 +11,7 @@ if number < 0:
     last_digit = number % -10
 
 if last_digit > 5:
-    print(f" {str1} {number:d} is {last_digit:d} {geater}")
+    print(f"{str1} {number:d} is {last_digit:d} {greater}")
 elif last_digit == 0:
     print(f"{str1} {number:d} is {last_digit:d} {zero}")
 else:
